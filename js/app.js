d53e4de33ffe436a2b3c598a926342277ec88704
kouuch <rifkyirsada2@gmail.com>
2025-03-17 22:40:15 +0800

    active section

--- js/app.js
+++ js/app.js
@@ -23,3 +23,15 @@ window.addEventListener('load', () => {
         })
     })
 })
+// active section
+const sectionNavigator = name => {
+    let sections = document.querySelectorAll('section')
+    let header = decoment.querySelector('header')
+    sections.forEach(section => {
+        section.classList.remove('section-show')
+        if (section.classList.contains(name)) {
+            section.classList.add('section-show')
+            header.classList.add('active')
+        }
+    })
+}
