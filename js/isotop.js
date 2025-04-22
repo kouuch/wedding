c377c989ab38ee8cd03fe4891b75be41b5e236dd
kouuch <rifkyirsada2@gmail.com>
2025-04-22 20:48:01 +0800

    listener function

--- js/isotop.js
+++ js/isotop.js
@@ -8,3 +8,17 @@ const select = (el, all = false) => {
         return document.querySelector(el)
     }
 }
+
+
+// event listener function
+
+const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+        if (all) {
+            selectEl.forEach(e => e.addEventListener(type, listener))
+        } else {
+            selectEl.addEventListener(type, listener)
+        }
+    }
+}
